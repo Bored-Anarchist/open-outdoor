5c20db8ba5422508bd3f180c185eb57263c3bcd4
Eric Posses <109712444+Bored-Anarchist@users.noreply.github.com>
2026-08-21 18:29:26 -0400

    feat: add Expo native spike module

---
+++ packages/native-spikes/ios/OpenOutdoorNativeSpikesModule.swift
@@ -0,0 +1,11 @@
+import ExpoModulesCore
+
+public final class OpenOutdoorNativeSpikesModule: Module {
+  public func definition() -> ModuleDefinition {
+    Name("OpenOutdoorNativeSpikes")
+
+    Constant("policyVersion") {
+      1
+    }
+  }
+}
